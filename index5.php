d0620492e86e7b6215d406a09b13106f51c0823b
Mattia21-byte <mattiago2105@gmail.com>
2026-03-26 18:24:14 +0100

    esercizio media numeri PHP

---
+++ index5.php
@@ -0,0 +1,24 @@
+<?php
+//Dato un array di numeri a scelta, scrivere un programma che calcoli la media solo dei numeri pari contenuti all’interno dell’array
+
+$numbers = [1, 12, 34, 36, 37, 58, 41, 6, 99, 102, 106, 3, 33, 31, 86, 88, 40, 71, 51, 32, 90];
+
+
+$sum = 0;
+$count = 0;
+
+foreach ($numbers as $num) {
+    if ($num % 2 == 0) {
+        $sum += $num;
+        $count++;
+    }
+}
+
+if ($count > 0) {
+    $media = $sum / $count;
+    echo "La media dei numeri pari è: " . $media;
+} else {
+    echo "Non ci sono numeri pari nell'array.";
+}
+
+?>
